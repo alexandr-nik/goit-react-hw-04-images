9b9013422a585f9bb64c2e28f7d276a338ad432d
Alexander <semovs@gmail.com>
2023-02-15 15:28:45 +0200

    edit App

--- src/components/ApiPixabay/ApiPixabay.js
+++ src/components/ApiPixabay/ApiPixabay.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 const PixabayKey = '32020206-eb1e8243d0555b8d860dcfe9c';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
+// axios.defaults.headers.common['Authorization'] = PixabayKey;
+axios.defaults.params = {
+  orientation: 'horizontal',
+  per_page: 12,
+  image_type: 'photo'
+};
 
 export const AxiosPaxabay = async (page, q) => {
   return await axios
     .get(
-      `?key=${PixabayKey}&page=${page}&per_page=12&q=${q}&image_type=photo&orientation=horizontal`
+      `?key=${PixabayKey}&page=${page}&per_page=12&q=${q}`
     )
     .then(data => data.data);
 };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,31 +20,45 @@ export function App() {
   };
 
   const searchImages = q => {
-    setQ('');
+    setQ(q);
     setPage(1);
     setGalleryList([]);
     setTotalHits(null);
   };
+  useEffect(() => {
+    if (totalHits === null) {
+      return;
+    }
+    if (totalHits === 0) {
+      errorMessage('Sorry. Image not found:(');
+      return;
+    } else {
+      infoMessage(`We found ${totalHits} images`);
+    }
+  }, [totalHits]);
 
   useEffect(() => {
     if (q === '') {
       return;
     }
-    try {
-      setLoader(true);
-      const { data, totalHits } = AxiosPaxabay(page, q);
-      if (totalHits === 0) {
-        errorMessage('Sorry. Image not found:(');
-        return;
-      } else {
-        infoMessage(`We found ${totalHits} images`);
+    setLoader(true);
+    const controller = new AbortController();
+    async function getImages() {
+      try {
+        await AxiosPaxabay(page, q, {
+          signal: controller.signal,
+        }).then(data => {
+          setGalleryList(prev => [...prev, ...data.hits]);
+          setTotalHits(data.totalHits);
+        });
+      } catch (error) {
+        errorMessage(error);
+      } finally {
+        setLoader(false);
       }
-      data.then(data => setGalleryList(prev => [...prev, ...data.hits]));
-    } catch (error) {
-      errorMessage(error);
-    } finally {
-      setLoader(false);
     }
+    getImages();
+    return controller.abort();
   }, [page, q]);
 
   return (
@@ -61,94 +75,3 @@ export function App() {
     </>
   );
 }
-
-//  class App extends PureComponent {
-//   state = {
-//     page: 1,
-//     q: '',
-//     galleryList: [],
-//     totalHits: null,
-//     loader: false,
-//   };
-//   componentDidUpdate(prevProps, prevState) {
-//     if (prevState.page !== this.state.page || prevState.q !== this.state.q) {
-//       this.getImage();
-//     }
-//     if (this.state.totalHits === 0) {
-//       errorMessage('Sorry. Image not found:(');
-//     }
-//     if (
-//       this.state.totalHits > 0 &&
-//       this.state.totalHits !== prevState.totalHits
-//     ) {
-//       infoMessage(`We found ${this.state.totalHits} images`);
-//     }
-//   }
-//   async getImage() {
-//     const { page, q } = this.state;
-//     this.setState({ loader: true });
-//     await AxiosPaxabay(page, q)
-//       .then(data => {
-//         this.setState(prevState => ({
-//           galleryList: [...prevState.galleryList, ...data.hits],
-//           totalHits: data.totalHits,
-//         }));
-//       })
-//       .finally(() => this.setState({ loader: false }));
-//   }
-//   loadMore = () => {
-//     this.setState(prevState => ({
-//       page: prevState.page + 1,
-//     }));
-//   };
-//   searchImages = q => {
-//     this.setState({
-//       q,
-//       page: 1,
-//       galleryList: [],
-//       totalHits: null,
-//     });
-//   };
-//   render() {
-//     const { galleryList, totalHits, page, loader } = this.state;
-//     return (
-//       <>
-//         <Searchbar getFindName={this.searchImages} />
-//         <ImageGallery>
-//           <ImageGalleryItem galleryList={galleryList} />
-//           {Math.ceil(totalHits / 12) > page && !loader && (
-//             <Button loadMore={this.loadMore} />
-//           )}
-//           {loader && <Loader />}
-//         </ImageGallery>
-//         <ToastContainer />
-//       </>
-//     );
-//   }
-// }
-
-// async componentDidUpdate(_, prevState) {
-//   const { query, page } = this.state;
-
-//   if (query !== prevState.query || page !== prevState.page) {
-//     try {
-//       this.setState({ isLoading: true });
-//       const { photos, total_results } = await ImageService.getImages(
-//         query,
-//         page
-//       );
-//       if (!photos.length) {
-//         this.setState({ isEmpty: true });
-//         return;
-//       }
-//       this.setState(prevState => ({
-//         photos: [...prevState.photos, ...photos],
-//         showBtn: page < Math.ceil(total_results / 15),
-//       }));
-//     } catch (error) {
-//       this.setState({ error: error.message });
-//     } finally {
-//       this.setState({ isLoading: false });
-//     }
-//   }
-// }
